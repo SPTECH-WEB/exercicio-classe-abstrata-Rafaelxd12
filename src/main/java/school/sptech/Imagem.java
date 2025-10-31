e77a6d079ef2d03c621ee3a184bbc2a8550f48ac
rafael <rafael.vieira@sptech.school>
2025-10-31 12:57:46 -0300

    Finalizado

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,44 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+        figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaAreas = 0.0;
+        for (Figura figura: figuras){
+            somaAreas += figura.calcularArea();
+        }
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasMaioresQue20 = new ArrayList<>();
+        for (Figura figura: figuras){
+            if (figura.calcularArea() > 20){
+                figurasMaioresQue20.add(figura);
+            }
+        }
+        return figurasMaioresQue20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> Quadrados = new ArrayList<>();
+        for (Figura figura: figuras){
+            if (figura instanceof Quadrado){
+                Quadrados.add(figura);
+            }
+        }
+        return Quadrados;
+    }
 }
--- src/main/java/school/sptech/Triangulo.java
+++ src/main/java/school/sptech/Triangulo.java
@@ -1,4 +1,36 @@
 package school.sptech;
 
-public class Triangulo {
+public class Triangulo extends Figura{
+    private Double base, altura;
+
+    public Triangulo() {}
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return (base * altura)/2;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo{" +
+                "base=" + base +
+                ", altura=" + altura +
+                '}';
+    }
 }
